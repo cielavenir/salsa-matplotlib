2b260306fb4ecf5ae5b679ab0fede1e39739e7c0
Vittorio Palmisano <vpalmisano@gmail.com>
2005-10-15 22:42:01 +0200

    Imported Debian patch 0.82-1

---
+++ debian/mkcopyright.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+CURDIR="`dirname $0`"
+
+echo "This package was debianized by Vittorio Palmisano <vpalmisano@gmail.com> on" > "$CURDIR"/copyright
+	echo -e "`date -R`\n" >> "$CURDIR"/copyright
+	echo -e  "It was downloaded from http://matplotlib.sourceforge.net/\n" >> "$CURDIR"/copyright
+	echo -e  "Upstream Author: John D. Hunter  <jdhunter@ace.bsd.uchicago.edu>\n" >> "$CURDIR"/copyright
+	echo -e  "Copyright:\n" >> "$CURDIR"/copyright
+
+	for i in `ls "$CURDIR"/../license`; do
+		echo "********************************************************************************" >> "$CURDIR"/copyright;
+		echo "$i" >> "$CURDIR"/copyright;
+		cat "$CURDIR"/../license/"$i" >> "$CURDIR"/copyright;
+		echo -e "\n" >> "$CURDIR"/copyright;
+	done
+
